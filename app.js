ec7817ef3ed344058c84a3593406c46c50b21485
uniquetree <uniquecodingtree@gmail.com>
2016-02-16 11:04:28 +0800

    完善项目bower、gulp、webpack开发工具的配置。

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-//var admin = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app'));
--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var notify = require('gulp-notify');
 var livereload = require('gulp-livereload');
@@ -19,8 +20,16 @@ var webpack = require('webpack-stream');
 // 清理任务
 gulp.task('clean', function(){
 
-    return gulp.src(['./app/styles/', './app/scripts/', './app/images/'], {read: false})
-        .pipe(clean());
+    return gulp.src(['./app/libs/', './app/styles/', './app/scripts/', './app/images/'], {read: false})
+        .pipe(clean())
+        .pipe(notify({ message: 'Clean task complete' }));
+});
+
+gulp.task('copy', function(){
+    gulp.src('./bower_components/angular/angular.min.js')
+        .pipe('./app/libs/');
+    //gulp.src('./bower_components/angular/angular.min.js')
+    //    .pipe('./app/libs/');
 });
 
 // 图片处理任务
@@ -32,20 +41,30 @@ gulp.task('image', function(){
         .pipe(notify({ message: 'Images task complete' }));
 });
 
-// 样式处理任务
+// 样式处理任务,编译/压缩/合并
 gulp.task('sass', function () {
 
-    return gulp.src('./src/styles/**/*.scss')
+    // app页面的样式文件
+    gulp.src('./src/styles/app/**/*.scss')
         .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('./app/styles/'))
+        .pipe(concat('app.css'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
         .pipe(gulp.dest('./app/styles/'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(notify({ message: 'App-Styles task complete' }));
+    // admin页面的样式文件
+    gulp.src('./src/styles/admin/**/*.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(concat('admin.css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./app/styles/'))
+        .pipe(notify({ message: 'Admin-Styles task complete' }));
 });
 
-// js脚本处理任务
+// js脚本处理任务,这里进行webpack处理
 gulp.task('script', ['image', 'sass'], function(){
 
     return gulp.src('./src/scripts/**/*.js')
